d025157a4a3340b3f5e21c31c96df4c794fbbcf6
evertvankammen <evert@evkammen.nl>
2023-03-01 22:43:21 +0100

    model toegevoegd

--- src/experimental/__init__.py
+++ src/experimental/__init__.py
@@ -1,5 +1,23 @@
-from src import parallelizer, configurator
+import logging
+from datetime import datetime
+
+from src import configurator
+from src.experimental.models import GhSearchSelection
+
+
+def initialize():
+    dt = datetime.now()
+    logging.basicConfig(filename='../log/main.' + str(dt) + '.log',
+                        format='%(asctime)s %(levelname)s: %(message)s',
+                        level=logging.INFO, encoding='utf-8')
+    configurator.inifile = '../../var/commitextractor.ini'
+
+
+def execute():
+    ghs = GhSearchSelection.get(GhSearchSelection.id == 1)
+    print(ghs.name)
+
 
 if __name__ == '__main__':
-    configurator.inifile = '../../var/commitextractor.ini'
-    parallelizer.start_processen()
+    initialize()
+    execute()
---
+++ src/experimental/models.py
@@ -0,0 +1,52 @@
+from peewee import CharField, DateField, Model, AutoField, BooleanField, \
+    IntegerField, IPField, DateTimeField, SQL, PostgresqlDatabase
+
+from src import configurator
+
+configurator.inifile = '../../var/commitextractor.ini'
+params = configurator.get_database_configuration()
+pg_db = PostgresqlDatabase('multicore', user=params.get('user'), password=params.get('password'),
+                           host='localhost', port=5432)
+
+
+class BaseModel(Model):
+    """A base model that will use our Postgresql database"""
+
+    class Meta:
+        database = pg_db
+        schema = 'test'
+
+
+class GhSearchSelection(BaseModel):
+    id = AutoField()
+    name = CharField(null=True)
+    is_fork = BooleanField(null=True)
+    commits = IntegerField(null=True)
+    branches = IntegerField(null=True)
+    default_branch = CharField(null=True)
+    releases = IntegerField(null=True)
+    contributors = IntegerField(null=True)
+    license = CharField(null=True)
+    watchers = IntegerField(null=True)
+    stargazers = IntegerField(null=True)
+    forks = IntegerField(null=True)
+    size = IPField(null=True)
+    created_at = DateTimeField(null=True)
+    pushed_at = DateTimeField(null=True)
+    updated_at = DateTimeField(null=True)
+    homepage = CharField(null=True)
+    main_language = CharField(null=True)
+    total_issues = IntegerField(null=True)
+    open_issues = IntegerField(null=True)
+    total_pull_requests = IntegerField(null=True)
+    open_pull_requests = IntegerField(null=True)
+    last_commit = DateTimeField(null=True)
+    last_commit_sha = CharField(null=True)
+    has_wiki = BooleanField(null=True)
+    is_archived = BooleanField(null=True)
+    import_date = DateField(null=True, constraints=[SQL('DEFAULT CURRENT_DATE')])
+    sub_study = CharField(null=True)
+
+
+pg_db.connect()
+pg_db.create_tables([GhSearchSelection])
