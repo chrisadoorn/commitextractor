d09b60d366edbed0f9f328a58d065ebb62ac5b41
evertvankammen <evert@evkammen.nl>
2023-03-22 23:18:53 +0100

    zoeken naar cc primitives

--- src/webapplication/app.py
+++ src/webapplication/app.py
@@ -20,17 +20,85 @@ def hello_world():
 @app.route("/selected/")
 def form_gh_search():
     ghs = GhSearchSelection.select().where(GhSearchSelection.selected_for_survey)
-    return render_template('selected.html', latest_selection_list=ghs)
+    return render_template("selected.html", latest_selection_list=ghs)
 
 
 @app.route("/showgithub/<select_id>/detail/")
 def form_commits_for_projects(select_id):
     selection = GhSearchSelection.select().where(GhSearchSelection.id == select_id)
     commits = CommitInformation.select().where(CommitInformation.id_project == select_id)
-    return render_template('detail.html', commits=commits, selection=selection)
+    for commit in commits:
+        commit.selections_with_mc = get_files(commit.id)
+    return render_template("detail.html", commits=commits, selection=selection)
 
 
 @app.route("/showgithub/<commit_id>/files/")
 def files(commit_id):
+    selections_with_mc = get_files(commit_id)
+    return render_template("files.html", selections=selections_with_mc)
+
+
+def get_files(commit_id):
     selections = FileChanges.select().where(FileChanges.id_commit == commit_id)
-    return render_template('files.html', selections=selections)
+    selections_with_mc = []
+    for sel in selections:
+        t = analyse(sel.text_after)
+        sel.text_after_analysed = t[0]
+        sel.multicore_found = t[1]
+        if len(sel.multicore_found) > 0:
+            selections_with_mc.append(sel)
+    return selections_with_mc
+
+
+def analyse(text_to_analyse):
+    i = 0
+    text_after_analysed = ""
+    found_mc = ""
+    try:
+        for line in text_to_analyse.splitlines():
+            text = is_what(line)
+            if len(text) > 0:
+                found_mc = found_mc + "{} {} \n".format(i, text)
+            text_after_analysed = text_after_analysed + "{} {}\n".format(i, line)
+            i = i + 1
+        return text_after_analysed, found_mc
+    except AttributeError:
+        return "", ""
+
+
+def is_what(text):
+    try:
+        index_comment = text.index("#")
+    except ValueError:
+        index_comment = len(text)
+    remarks = ""
+    for f in functions:
+        try:
+            found_index = text.index(f)
+        except ValueError:
+            found_index = -1
+        if 0 < found_index < index_comment:
+            remarks = remarks + f
+
+    return remarks
+
+
+functions = ["spawn",
+             "spawn_link",
+             "spawn_monitor",
+             "self",
+             "send",
+             "receive",
+             "Agent",
+             "Application",
+             "Config",
+             "Config.Provider",
+             "Config.Reader",
+             "DynamicSupervisor",
+             "GenServer",
+             "Node",
+             "Process",
+             "Registry",
+             "Supervisor",
+             "Task",
+             "Task.Supervisor"]
