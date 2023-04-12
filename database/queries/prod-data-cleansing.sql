86014947fcc4a8d1c14d761a59042e39e2a313c4
chrisadoorn <chris.adoorn@gmail.com>
2023-04-12 21:13:12 +0200

    Script om commitinfo tabel te schonen van commits die geen bestandswijziging bevatten.

--- database/queries/prod-data-cleansing.sql
+++ database/queries/prod-data-cleansing.sql
@@ -1,21 +1,46 @@
 -- vervang irritante enkel quote in de naam van een programmeertaal
-update prod.project
+update project
 set languages = replace(languages, 'Cap''n', 'Capn')
 where languages like '%Cap%';
 
--- vervang enkele quotes voor namen 
+-- vervang enkele quotes binnen de json string met languages
 update project
 set languages = replace(languages,'''', '"' )
 ;
 
--- selecteer een aantal metrieken
-SELECT id, naam, 
-main_language, 
-cast(languages::json->>'Java' as int) as java_bytes,
-cast(languages::json->>'Java' as int) / 1024  as java_kb,
-project_size as project_kb, 
-contributors,
-number_of_languages, 
-languages
-	FROM project	
-	order by number_of_languages desc;
+----------------------------
+-- schoon commitinfo
+-----------------------------
+-- stap 1: create view om te weten wat geschoond moet worden.
+CREATE MATERIALIZED VIEW IF NOT EXISTS prod.commit_met_inhoud
+TABLESPACE pg_default
+AS
+ SELECT DISTINCT b.idcommit
+   FROM prod.bestandswijziging b
+  ORDER BY b.idcommit
+WITH DATA;
+
+ALTER TABLE IF EXISTS prod.commit_met_inhoud
+    OWNER TO postgres;
+
+
+CREATE UNIQUE INDEX idx_commit_met_inhoud_pk
+    ON prod.commit_met_inhoud USING btree
+    (idcommit)
+    TABLESPACE pg_default;
+
+
+-- stap 2
+-- controle hoeveel rijen hebben we?
+-- het verschil wordt geschoond
+select count(*) from commit_met_inhoud; -- 86790
+select count(*) from commitinfo c;      -- 131518
+
+select count(id) from commitinfo c
+where c.id not in
+( select v.idcommit from commit_met_inhoud v); -- 44728
+
+-- stap 3 opschonen.
+delete from commitinfo c
+where c.id not in
+( select v.idcommit from commit_met_inhoud v); -- 44728
--- database/queries/queries_on_prod.sql
+++ database/queries/queries_on_prod.sql
@@ -2,6 +2,20 @@ select id
 from commitinfo c 
 where c.id not in ( select distinct(idcommit) from bestandswijziging b)
 
+-- selecteer een aantal metrieken
+SELECT id, naam,
+main_language,
+cast(languages::json->>'Java' as int) as java_bytes,
+cast(languages::json->>'Java' as int) / 1024  as java_kb,
+project_size as project_kb,
+contributors,
+number_of_languages,
+languages
+	FROM project
+	order by number_of_languages desc;
+
+
+
 alter or create materialized view commit_met_inhoud as
 	 select distinct(idcommit) as idcommit from bestandswijziging b
 	 order by idcommit
