ef55bc7721217a396ed94da8dffd4e7c277f176b
evkammen <evert@evkammen.nl>
2023-04-12 19:38:00 +0200

    alles werk nu vanuit 1 file

--- src/process_selected_repos/main.py
+++ src/process_selected_repos/main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from src.models.models import GhSearchSelection, pg_db, CommitInfo, BestandsWijziging, Selectie, Project, \
     ManualChecking, CommitAuthorInformation, ProjectsProcessedForAuthors, ProjectAuthorInformation, pg_db_schema
 from src.repo_extractor.commitextractor import extract_repository
-from src.requester.api_requester import get_author_data, get_author_data_one_commit
+from src.requester.api_requester import get_author_data, get_author_data_one_commit, fetch_authors_per_commit
 
 dt = datetime.now()
 filename = \
@@ -135,38 +135,6 @@ def fetch_authors_per_project(limit=5):
         processed.processed = True
         processed.error_description = error
         processed.save()
-
-
-def fetch_authors_per_commit(limit=5):
-    schema = pg_db_schema
-    cursor = pg_db.execute_sql(
-        "SELECT ci.idproject, ci.emailaddress, ci.username, ci.hashvalue, pr.naam "
-        "FROM " + schema + ".commitinfo AS ci " +
-        "JOIN " + schema + ".project AS pr ON ci.idproject = pr.id " +
-        "WHERE author_id is null limit({});".format(limit)
-    )
-    for (id_project, email_address_hashed, username_hashed, sha, project_name) in cursor.fetchall():
-        try:
-            existing_commit_info = CommitInfo().select().where(
-                CommitInfo.idproject == id_project,
-                CommitInfo.username == username_hashed,
-                CommitInfo.emailaddress == email_address_hashed,
-                CommitInfo.author_id.is_null(False)).get()
-            print(
-                "[update] " + project_name + ", un:" + username_hashed + ", ea:" + email_address_hashed)
-            update_commit_info(id_project, sha, existing_commit_info.author_id, existing_commit_info.author_login)
-        except CommitInfo.DoesNotExist:
-            print("[New] " + project_name + ", un:" + username_hashed + ", ea:" + email_address_hashed)
-            (commit_sha, author_login, author_id), error = get_author_data_one_commit(project_name, sha)
-            update_commit_info(id_project, sha, author_id, author_login)
-
-
-def update_commit_info(id_project, sha, author_id, author_login):
-    to_update_commit_info = CommitInfo().select().where(
-        CommitInfo.idproject == id_project, CommitInfo.hashvalue == sha).get()
-    to_update_commit_info.author_id = author_id
-    to_update_commit_info.author_login = author_login
-    to_update_commit_info.save()
 
 
 if __name__ == '__main__':
