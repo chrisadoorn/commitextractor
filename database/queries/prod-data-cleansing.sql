b5f79a4b977cc8a1126c190806fd0f954a6bb092
chrisadoorn <chris.adoorn@gmail.com>
2023-04-13 20:17:18 +0200

    Snellere methode gemaakt om de commitinfo records te schonen.

--- database/queries/prod-data-cleansing.sql
+++ database/queries/prod-data-cleansing.sql
@@ -11,36 +11,45 @@ set languages = replace(languages,'''', '"' )
 ----------------------------
 -- schoon commitinfo
 -----------------------------
--- stap 1: create view om te weten wat geschoond moet worden.
-CREATE MATERIALIZED VIEW IF NOT EXISTS prod.commit_met_inhoud
-TABLESPACE pg_default
-AS
- SELECT DISTINCT b.idcommit
-   FROM prod.bestandswijziging b
-  ORDER BY b.idcommit
-WITH DATA;
+-- stap 1: create table om te weten wat geschoond moet worden.
+CREATE TABLE id_in_use (
+	idcommit int8 NOT NULL
+);
 
-ALTER TABLE IF EXISTS prod.commit_met_inhoud
-    OWNER TO postgres;
+-- Permissions
 
+ALTER TABLE id_in_use OWNER TO postgres;
+GRANT ALL ON TABLE id_in_use TO postgres;
 
-CREATE UNIQUE INDEX idx_commit_met_inhoud_pk
-    ON prod.commit_met_inhoud USING btree
-    (idcommit)
-    TABLESPACE pg_default;
+insert into id_in_use(idcommit)
+SELECT DISTINCT b.idcommit
+   FROM bestandswijziging b;
+
+CREATE UNIQUE INDEX idx_bestandswijzging_idcommit
+    ON id_in_use USING btree
+    (idcommit ASC NULLS LAST)
+;
+
+ALTER TABLE IF EXISTS id_in_use
+    CLUSTER ON idx_bestandswijzging_idcommit;
 
 
 -- stap 2
 -- controle hoeveel rijen hebben we?
 -- het verschil wordt geschoond
-select count(*) from commit_met_inhoud; -- 86790
+select count(*) from id_in_use; -- 86790
 select count(*) from commitinfo c;      -- 131518
 
 select count(id) from commitinfo c
 where c.id not in
-( select v.idcommit from commit_met_inhoud v); -- 44728
+( select v.idcommit from id_in_use v); -- 44728
 
 -- stap 3 opschonen.
 delete from commitinfo c
 where c.id not in
-( select v.idcommit from commit_met_inhoud v); -- 44728
+( select v.idcommit from id_in_use v); -- 44728
+
+-- stap 4 opruimen
+
+	drop table id_in_use;
+
