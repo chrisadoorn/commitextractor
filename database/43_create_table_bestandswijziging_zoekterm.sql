115580f79b97a523b5b0530a5442968f440e1362
chrisadoorn <chris.adoorn@gmail.com>
2023-04-15 14:48:16 +0200

    derde analyse tabel toegevoegd

---
+++ database/43_create_table_bestandswijziging_zoekterm.sql
@@ -0,0 +1,32 @@
+set schema 'test';
+
+-- Table: bestandswijziging_zoekterm
+
+DROP TABLE IF EXISTS bestandswijziging_zoekterm;
+
+CREATE TABLE IF NOT EXISTS bestandswijziging_zoekterm
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    idbestandswijziging bigint NOT NULL,
+    zoekterm character varying NOT NULL,
+    falsepositive boolean DEFAULT false,
+    regelnummers integer[],
+    aantalgevonden integer DEFAULT 0,
+    CONSTRAINT bestandswijziging_zoekterm_pk PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE bestandswijziging_zoekterm ADD CONSTRAINT bestandswijziging_fk FOREIGN KEY (idbestandswijziging) REFERENCES bestandswijziging(id) ON DELETE CASCADE;
+
+
+ALTER TABLE IF EXISTS bestandswijziging_zoekterm
+    OWNER to postgres;
+
+GRANT DELETE, INSERT, SELECT, UPDATE ON TABLE bestandswijziging_zoekterm TO appl;
+
+GRANT ALL ON TABLE bestandswijziging_zoekterm TO postgres;
+
+COMMENT ON TABLE bestandswijziging_zoekterm
+    IS 'analyse info over voorkomen van een zoekterm in een bestandswijziging';
+
