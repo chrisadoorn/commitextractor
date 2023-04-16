6f7e8ff85fef1c9527c2d0bbbb4774e195905b12
evkammen <evert@evkammen.nl>
2023-04-16 14:25:56 +0200

    read-diff

--- src/utils/read_diff.py
+++ src/utils/read_diff.py
@@ -72,41 +72,33 @@ class ReadDiff:
     def read_diff_text(self, chunk=''):
         self.lines = chunk.splitlines()
         if len(self.lines) == 0:
-            return "No lines found"
+            return [], []
         self.new_lines = []
         self.removed_lines = []
         self.linecounter = (0, 0)
         for line in self.lines:
             self.__process_line(line)
-
-        print("Aantal aangepaste regels: " + str(len(self.new_lines) + len(self.removed_lines)))
-        print("Nieuwe regels:")
-        for l1 in self.new_lines:
-            if l1[2]:
-                print(l1)
-        print("Verwijderde regels:")
-        for l2 in self.removed_lines:
-            if l2[2]:
-                print(l2)
+        return self.new_lines, self.removed_lines
 
     def __process_line(self, line):
         if line.startswith('@@'):
             self.linecounter = self.__process_chunk_line(line)
         elif line.startswith('-'):
-            (line, is_mc) = self.__clean_diff_line(line)
+            (line, is_mc) = self.__clean_and_analyse_line(line)
             if len(line) > 0:
                 item = (self.linecounter[0], line, is_mc)
-                self.removed_lines.append(item)
+                if item[2]:
+                    self.removed_lines.append(item)
             self.linecounter = (self.linecounter[0] + 1, self.linecounter[1])
         elif line.startswith('+'):
-            (line, is_mc) = self.__clean_diff_line(line)
+            (line, is_mc) = self.__clean_and_analyse_line(line)
             if len(line) > 0:
                 item = (self.linecounter[1], line, is_mc)
-                self.new_lines.append(item)
+                if item[2]:
+                    self.new_lines.append(item)
             self.linecounter = (self.linecounter[0], self.linecounter[1] + 1)
         else:
             self.linecounter = (self.linecounter[0] + 1, self.linecounter[1] + 1)
-        print(self.linecounter)
 
     @staticmethod
     def __process_chunk_line(line):
@@ -115,7 +107,7 @@ class ReadDiff:
         split_chunk_parts = [word.strip() for word in split_on_at[0].split(' ')]
         return int(split_chunk_parts[0].split(',')[0][1:]), int(split_chunk_parts[1].split(',')[0][1:])
 
-    def __clean_diff_line(self, line):
+    def __clean_and_analyse_line(self, line):
         pos_comment = line.find(self.single_line_comment)
         if pos_comment > -1:
             line = line[:pos_comment]
@@ -124,35 +116,56 @@ class ReadDiff:
         return line, is_mc
 
     def __is_mc_line(self, line):
-        for mc_indicator in self.mc_indicators:
-            r1 = re.search(r'(.*)' + mc_indicator + '(.*)', line, re.U)
-            if r1 is not None:
-                return True
-
-        return False
+        mc_found = []
+        temp_line = re.sub("\"", " QUOTE ", line)
+        words = re.sub("[^a-zA-Z] ", " ", temp_line).split()
+        start_quote_found = False
+        for word in words:
+            stripped_word = word.strip()
+            if not start_quote_found and stripped_word == "QUOTE":
+                start_quote_found = True
+                continue
+            if start_quote_found and stripped_word == "QUOTE":
+                start_quote_found = False
+            if start_quote_found:
+                continue
+            if stripped_word in self.mc_indicators:
+                mc_found.append(stripped_word)
+        return mc_found
 
 
 if __name__ == '__main__':
     try:
-        limit = 100_000
+        limit = 1000
         initialize()
         logging.info('Started at:' + str(datetime.now()))
         schema = pg_db_schema
         read_diff = ReadDiff(language=Language.ELIXIR)
         sql = \
-            "select bw.id, bw.idcommit, bw.locatie, bw.filename, bw.difftext from " + schema + \
-            ".bestandswijziging as bw where difftext is not null limit({});".format(limit) \
+            "select bw.id, bw.idcommit, bw.locatie, bw.filename, bw.difftext, bw.tekstachteraf from " + schema + \
+            ".bestandswijziging as bw where difftext is not null and extensie <> '.md' order by bw.id limit({});". \
+            format(limit) \
             if limit is not None else \
-            "select bw.id, bw.idcommit, bw.locatie, bw.filename, bw.difftext from " + schema + \
-            ".bestandswijziging as bw where difftext is not null;"
+            "select bw.id, bw.idcommit, bw.locatie, bw.filename, bw.difftext, bw.tekstachteraf from " + schema + \
+            ".bestandswijziging as bw where difftext is not null and extensie <> '.md' order by bw.id;"
 
         cursor = pg_db.execute_sql(sql)
         counter = 1
-        for (bestandswijziging_id, id_commit, locatie, filename, diff_text) in cursor.fetchall():
+        for (bestandswijziging_id, id_commit, locatie, filename, diff_text, tekstachteraf) in cursor.fetchall():
             print("counter: " + str(counter))
             print("id: " + str(bestandswijziging_id))
             print("filename: " + filename)
-            read_diff.read_diff_text(diff_text)
+            (new_lines, old_lines) = read_diff.read_diff_text(diff_text)
+            if len(new_lines) > 0:
+                print("new lines: " + str(len(new_lines)))
+                print(new_lines)
+            else:
+                print("new lines: 0")
+            if len(old_lines) > 0:
+                print("old lines: " + str(len(old_lines)))
+                print(old_lines)
+            else:
+                print("old lines: 0")
             print("--------------------")
             counter += 1
 
