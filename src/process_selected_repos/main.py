d6e10f464e9f655e3ce3855b37af5876742468d4
evkammen <evert@evkammen.nl>
2023-04-18 11:11:15 +0200

    read-diff nu onafhankelijke klasse

--- src/process_selected_repos/main.py
+++ src/process_selected_repos/main.py
@@ -1,11 +1,12 @@
 import logging
 import os
 from datetime import datetime
-
+from peewee import *
 from src.models.models import GhSearchSelection, pg_db, CommitInfo, BestandsWijziging, Selectie, Project, \
-    ManualChecking
+    ManualChecking, pg_db_schema, TempDiffTextAnalysis
 from src.repo_extractor.commitextractor import extract_repository
 from src.requester.api_requester import fetch_authors_per_commit
+from src.utils.read_diff import ReadDiff, Language
 
 dt = datetime.now()
 filename = \
@@ -21,7 +22,7 @@ def initialize():
 
 def create_tables():
     pg_db.create_tables([GhSearchSelection, Selectie, Project, CommitInfo, BestandsWijziging, ManualChecking,
-                        ],
+                         TempDiffTextAnalysis],
                         safe=True)
 
 
@@ -64,14 +65,88 @@ def process_repos(subproject):
         t.save()
 
 
+def __save_temp_diff_text_analysis(bestandswijzigingid, file_name, location, line_number, line_text, primitives,
+                                   type_of_diff, author_id, project_name, commitdatumtijd):
+    if TempDiffTextAnalysis.select().where(TempDiffTextAnalysis.idbestandswijziging == bestandswijzigingid,
+                                           TempDiffTextAnalysis.line_number == line_number).exists():
+        return
+
+    temp_diff_text_analysis = TempDiffTextAnalysis()
+    temp_diff_text_analysis.idbestandswijziging = bestandswijzigingid
+    temp_diff_text_analysis.filename = file_name
+    temp_diff_text_analysis.location = location
+    temp_diff_text_analysis.line_number = line_number
+    temp_diff_text_analysis.line_text = line_text
+    temp_diff_text_analysis.primitives = primitives
+    temp_diff_text_analysis.type_of_diff = type_of_diff
+    temp_diff_text_analysis.project_name = project_name
+    temp_diff_text_analysis.author_id = author_id
+    temp_diff_text_analysis.commitdatumtijd = commitdatumtijd
+    temp_diff_text_analysis.save()
+
+
+def __list_to_string(s):
+    str1 = ""
+    i = 0
+    x = len(s)
+    for ele in s:
+        if 0 < i < x:
+            str1 += ","
+        str1 += ele
+        i += 1
+    return str1
+
+
+def __analyse_diffs(limit=1000):
+    result = TempDiffTextAnalysis.select(fn.MAX(TempDiffTextAnalysis.idbestandswijziging)).scalar()
+    if result is None:
+        result = 0
+
+    read_diff = ReadDiff(language=Language.ELIXIR)
+    sql = "select bw.id, bw.idcommit, bw.locatie, bw.filename, bw.difftext, ci.author_id, pr.naam,ci.commitdatumtijd " \
+          "from {sch}.bestandswijziging as bw " \
+          "join {sch}.commitinfo as ci on bw.idcommit = ci.id " \
+          "join {sch}.project as pr on ci.idproject = pr.id " \
+          "where bw.difftext is not null and bw.extensie <> '.md' and bw.id >= {idva} order by bw.id " \
+          "limit({li});".format(sch=pg_db_schema, idva=result, li=limit)
+
+    cursor = pg_db.execute_sql(sql)
+    counter = 1
+    for (bestandswijziging_id, id_commit, locatie, filename, diff_text, author_id, pr_name,
+         commitdatumtijd) in cursor.fetchall():
+        print("counter: " + str(counter))
+        print("id: " + str(bestandswijziging_id))
+        print("filename: " + filename)
+        (new_lines, old_lines) = read_diff.read_diff_text(diff_text)
+        if len(new_lines) > 0:
+            print("new lines: " + str(len(new_lines)))
+            print(new_lines)
+            for (lijnnummer, tekst, primitives) in new_lines:
+                __save_temp_diff_text_analysis(bestandswijziging_id, filename, locatie, lijnnummer, tekst,
+                                               __list_to_string(primitives), 1, author_id, pr_name, commitdatumtijd)
+        else:
+            print("new lines: 0")
+        if len(old_lines) > 0:
+            print("old lines: " + str(len(old_lines)))
+            print(old_lines)
+            for (lijnnummer, tekst, primitives) in old_lines:
+                __save_temp_diff_text_analysis(bestandswijziging_id, filename, locatie, lijnnummer, tekst,
+                                               __list_to_string(primitives), -1, author_id, pr_name, commitdatumtijd)
+        else:
+            print("old lines: 0")
+        print("--------------------")
+        counter += 1
+
+
 if __name__ == '__main__':
     try:
         initialize()
         logging.info('Started at:' + str(datetime.now()))
-        # create_tables()
+        create_tables()
         # GhSearchSampleRequester.get_sample('Elixir')
         # process_repos('Elixir')
-        fetch_authors_per_commit()
+        # fetch_authors_per_commit()
+        __analyse_diffs(1_000_000)
         logging.info('Finished at:' + str(datetime.now()))
     except Exception as e:
         logging.error('Crashed at:' + str(datetime.now()))
--- src/utils/read_diff.py
+++ src/utils/read_diff.py
@@ -1,50 +1,16 @@
-import logging
-import os
 import re
-from peewee import *
-from datetime import datetime
 from enum import Enum
-
-from src.models.models import pg_db_schema, pg_db, TempDiffTextAnalysis
 
 JAVA_SINGLE_LINE_COMMENT = '//'  # Java single line comment
 ELIXIR_SINGLE_LINE_COMMENT = '#'  # Elixir single line comment
 RUST_SINGLE_LINE_COMMENT = '//'  # Rust single line comment
 
-ELIXIR_MC_INDICATOR = ["spawn",
-                       "spawn_link",
-                       "spawn_monitor",
-                       "self",
-                       "send",
-                       "receive",
-                       "Agent",
-                       "Application",
-                       "Config",
-                       "Config.Provider",
-                       "Config.Reader",
-                       "DynamicSupervisor",
-                       "GenServer",
-                       "Node",
-                       "Process",
-                       "Registry",
-                       "Supervisor",
-                       "Task",
-                       "Task.Supervisor", "Mix.Config"]
+ELIXIR_MC_INDICATOR = ["spawn", "spawn_link", "spawn_monitor", "self", "send", "receive", "Agent", "Application",
+                       "Config", "Config.Provider", "Config.Reader", "DynamicSupervisor", "GenServer", "Node",
+                       "Process", "Registry", "Supervisor", "Task", "Task.Supervisor", "Mix.Config"]
 
 JAVA_MC_INDICATOR = ["Thread"]
 RUST_MC_INDICATOR = ["Thread"]
-
-
-def initialize():
-    logging.basicConfig(filename=os.path.realpath(os.path.join(os.path.dirname(__file__),
-                                                               '..', '..', 'log',
-                                                               'main.' + str(datetime.now()) + '.log')),
-                        format='%(asctime)s %(levelname)s: %(message)s',
-                        level=logging.INFO, encoding='utf-8')
-
-
-def create_tables():
-    pg_db.create_tables([TempDiffTextAnalysis], safe=True)
 
 
 class Language(Enum):
@@ -117,20 +83,26 @@ class ReadDiff:
         return int(split_chunk_parts[0].split(',')[0][1:]), int(split_chunk_parts[1].split(',')[0][1:])
 
     def __clean_and_analyse_line(self, line):
+        line = line[1:].strip()
         pos_comment = line.find(self.single_line_comment)
-        if pos_comment > -1:
+        if pos_comment >= 0:
             line = line[:pos_comment]
-        line = line[2:].strip()
-        mc_found = self.__find_all_primitives(line)
+        if len(line) > 0:
+            mc_found = self.__find_all_primitives(line)
+        else:
+            mc_found = []  # empty line, no code
         return line, mc_found
 
     def __find_all_primitives(self, line):
         mc_found = []
         temp_line = re.sub("\"", " QUOTE ", line)
+        # vervang alles dat niet een letter, punt of underscore is door een spatie
+        # en split vervolgens op spaties
         words = re.sub("[^a-zA-Z._] ", " ", temp_line).split()
         start_quote_found = False
         for word in words:
             stripped_word = word.strip()
+            # check of tekst zich binnen een string literal bevindt
             if not start_quote_found and stripped_word == "QUOTE":
                 start_quote_found = True
                 continue
@@ -141,89 +113,3 @@ class ReadDiff:
             if stripped_word in self.mc_indicators:
                 mc_found.append(stripped_word)
         return mc_found
-
-
-def save_temp_diff_text_analysis(bestandswijzigingid, file_name, location, line_number, line_text, primitives,
-                                 type_of_diff, author_id, project_name, commitdatumtijd):
-    if TempDiffTextAnalysis.select().where(TempDiffTextAnalysis.idbestandswijziging == bestandswijzigingid,
-                                           TempDiffTextAnalysis.line_number == line_number).exists():
-        return
-
-    temp_diff_text_analysis = TempDiffTextAnalysis()
-    temp_diff_text_analysis.idbestandswijziging = bestandswijzigingid
-    temp_diff_text_analysis.filename = file_name
-    temp_diff_text_analysis.location = location
-    temp_diff_text_analysis.line_number = line_number
-    temp_diff_text_analysis.line_text = line_text
-    temp_diff_text_analysis.primitives = primitives
-    temp_diff_text_analysis.type_of_diff = type_of_diff
-    temp_diff_text_analysis.project_name = project_name
-    temp_diff_text_analysis.author_id = author_id
-    temp_diff_text_analysis.commitdatumtijd = commitdatumtijd
-    temp_diff_text_analysis.save()
-
-
-def list_to_string(s):
-    str1 = ""
-    i = 0
-    x = len(s)
-    for ele in s:
-        if 0 < i < x:
-            str1 += ","
-        str1 += ele
-        i += 1
-    return str1
-
-
-if __name__ == '__main__':
-    try:
-        limit = 1000
-        initialize()
-        create_tables()
-        logging.info('Started at:' + str(datetime.now()))
-        schema = pg_db_schema
-
-        result = TempDiffTextAnalysis.select(fn.MAX(TempDiffTextAnalysis.idbestandswijziging)).scalar()
-
-        if result is None:
-            result = 0
-
-        read_diff = ReadDiff(language=Language.ELIXIR)
-        sql = \
-            "select bw.id, bw.idcommit, bw.locatie, bw.filename, bw.difftext, ci.author_id, pr.naam, " \
-            "ci.commitdatumtijd from " + schema + ".bestandswijziging as bw " \
-            "join " + schema + ".commitinfo as ci on bw.idcommit = ci.id " \
-            "join " + schema + ".project as pr on ci.idproject = pr.id " \
-            "where difftext is not null and extensie <> '.md' and bw.id >= {0} order by bw.id " \
-            "limit({1});".format(result, limit)
-        cursor = pg_db.execute_sql(sql)
-        counter = 1
-        for (bestandswijziging_id, id_commit, locatie, filename, diff_text, author_id, pr_name,
-             commitdatumtijd) in cursor.fetchall():
-            print("counter: " + str(counter))
-            print("id: " + str(bestandswijziging_id))
-            print("filename: " + filename)
-            (new_lines, old_lines) = read_diff.read_diff_text(diff_text)
-            if len(new_lines) > 0:
-                print("new lines: " + str(len(new_lines)))
-                print(new_lines)
-                for (lijnnummer, tekst, primitives) in new_lines:
-                    save_temp_diff_text_analysis(bestandswijziging_id, filename, locatie, lijnnummer, tekst,
-                                                 list_to_string(primitives), 1, author_id, pr_name, commitdatumtijd)
-            else:
-                print("new lines: 0")
-            if len(old_lines) > 0:
-                print("old lines: " + str(len(old_lines)))
-                print(old_lines)
-                for (lijnnummer, tekst, primitives) in old_lines:
-                    save_temp_diff_text_analysis(bestandswijziging_id, filename, locatie, lijnnummer, tekst,
-                                                 list_to_string(primitives), -1, author_id, pr_name, commitdatumtijd)
-            else:
-                print("old lines: 0")
-            print("--------------------")
-            counter += 1
-
-        logging.info('Finished at:' + str(datetime.now()))
-    except Exception as e:
-        logging.error('Crashed at:' + str(datetime.now()))
-        logging.exception(e)
