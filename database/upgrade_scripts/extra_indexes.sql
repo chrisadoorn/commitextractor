3c9af098b7efa6c504f63cb4abe15a9dab6c1200
chrisadoorn <chris.adoorn@gmail.com>
2023-04-22 12:40:05 +0200

    extra index op kolom author_id voor tabel commitinfo toegevoegd

--- database/upgrade_scripts/extra_indexes.sql
+++ database/upgrade_scripts/extra_indexes.sql
@@ -2,3 +2,4 @@ SET schema 'test';
 
 CREATE INDEX commitinfo_idproject_idx ON commitinfo USING btree (idproject);
 CREATE INDEX bestandswijziging_idcommit_idx ON bestandswijziging USING btree (idcommit);
+CREATE INDEX commitinfo_idauthor_idx ON commitinfo USING btree (author_id);
