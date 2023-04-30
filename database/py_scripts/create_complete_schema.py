a28b5b0d8f76834fdd9d981cfdc0c38b97a8dcab
chrisadoorn <chris.adoorn@gmail.com>
2023-04-30 22:47:49 +0200

    script om gewijzigde databasse scripts weer samen te kunnen voegne voor een nieuw schema

---
+++ database/py_scripts/create_complete_schema.py
@@ -0,0 +1,56 @@
+import os
+
+from src.utils import db_postgresql
+
+scriptdir = os.path.dirname(__file__)
+
+print(str(scriptdir))
+run_dir = os.path.realpath(os.path.join(scriptdir, '../temp'))
+print(run_dir)
+sql_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+print(sql_dir)
+
+new_schema = 'demo'
+file_names = ['10_create_table_selectie.sql'
+    , '11_create_table_project.sql'
+    , '12_create_table_commitinfo.sql'
+    , '13_create_table_bestandswijziging.sql'
+    , '21_create_table_processor.sql'
+    , '21_create_table_processor.sql'
+    , '22_create_table_verwerk_project.sql'
+    , '23_create_table_verwerking_geschiedenis.sql'
+    , '31_create_procedure_registreer_processor.sql'
+    , '32_create_procedure_deregistreer_processor.sql'
+    , '33_create_procedure_registreer_verwerking.sql'
+    , '34_create_procedure_verwerk_volgend_project.sql'
+    , '41_create_table_zoekterm.sql'
+    , '42_create_table_bestandswijziging_info.sql'
+    , '42_create_table_bestandswijziging_info.sql'
+    , '51_create_table_java_zoekterm.sql'
+              ]
+
+os.makedirs(run_dir)
+full_script = os.path.realpath(os.path.join(run_dir, './' + 'full_script.' + new_schema + '.sql'))
+full = open(full_script, 'wt')
+for sql_file in file_names:
+    f_path = os.path.realpath(os.path.join(sql_dir, './' + sql_file))
+    org = open(f_path, 'rt')
+    n_path = os.path.realpath(os.path.join(run_dir, './' + sql_file))
+    new = open(n_path, 'wt')
+    full.write('------------------------------------------------------------------------------\n')
+    full.write('------------------------------------------------------------------------------\n')
+    full.write('--script--' + sql_file + '---------------------\n')
+    full.write('------------------------------------------------------------------------------\n')
+    full.write('------------------------------------------------------------------------------\n')
+    for line in org:
+        new.write(line.replace("'test'", "'" + new_schema + "'"))
+        full.write(line.replace("'test'", "'" + new_schema + "'"))
+    org.close()
+    new.close()
+
+
+full.close()
+# #    os.remove(n_path)
+#
+#
+# # os.rmdir(run_dir)
