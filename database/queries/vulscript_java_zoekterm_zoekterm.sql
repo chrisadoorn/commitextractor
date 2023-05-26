4ec609dde9c08509f387f121724fe0d082d2be21
chrisadoorn <chris.adoorn@gmail.com>
2023-05-26 11:23:40 +0200

    aanvullen java zoektermen. sampling voor java script bijgewerkt

--- database/queries/vulscript_java_zoekterm_zoekterm.sql
+++ database/queries/vulscript_java_zoekterm_zoekterm.sql
@@ -1,4 +1,4 @@
-set schema 'test';
+set schema 'qselect';
 
 INSERT INTO java_zoekterm (zoekterm,categorie,package,opmerking) VALUES
 	 ('synchronized','keywords',NULL,'(code block) or (methods)'),
@@ -16,6 +16,12 @@ INSERT INTO java_zoekterm (zoekterm,categorie,package,opmerking) VALUES
 	 ('Delayed','interfaces','java.util.concurrent',NULL),
 	 ('Executor','interfaces','java.util.concurrent',NULL),
 	 ('ExecutorService','interfaces','java.util.concurrent',NULL),
+	 ('Flow.Processor','interfaces','java.util.concurrent',NULL),
+	 ('Flow.Publisher','interfaces','java.util.concurrent',NULL),
+	 ('Flow.Subscriber','interfaces','java.util.concurrent',NULL),
+	 ('Flow.Subscription','interfaces','java.util.concurrent',NULL),
+	 
+	 
 	 ('ForkJoinPool.ForkJoinWorkerThreadFactory','interfaces','java.util.concurrent',NULL),
 	 ('ForkJoinPool.ManagedBlocker','interfaces','java.util.concurrent',NULL),
 	 ('Future','interfaces','java.util.concurrent',NULL),
@@ -26,10 +32,13 @@ INSERT INTO java_zoekterm (zoekterm,categorie,package,opmerking) VALUES
 	 ('ScheduledExecutorService','interfaces','java.util.concurrent',NULL),
 	 ('ThreadFactory','interfaces','java.util.concurrent',NULL),
 	 ('TransferQueue','interfaces','java.util.concurrent',NULL),
-	 ('Condition','interfaces','java.util.concurrent',NULL),
-	 ('Lock','interfaces','java.util.concurrent',NULL),
-	 ('ReadWriteLock','interfaces','java.util.concurrent',NULL),
+	 ('Condition','interfaces','java.util.concurrent.locks',NULL),
+	 ('Lock','interfaces','java.util.concurrent.locks',NULL),
+	 ('ReadWriteLock','interfaces','java.util.concurrent.locks',NULL),
 	 ('Thread','classes','java.lang',NULL),
+	 ('ThreadGroup','classes','java.lang',NULL),
+	 ('ThreadLocal','classes','java.lang',NULL),
+	 ('InheritableThreadLocal','classes','java.lang',NULL),
 	 ('AbstractExecutorService','classes','java.util.concurrent',NULL),
 	 ('ArrayBlockingQueue','classes','java.util.concurrent',NULL),
 	 ('CompletableFuture','classes','java.util.concurrent',NULL),
@@ -50,6 +59,7 @@ INSERT INTO java_zoekterm (zoekterm,categorie,package,opmerking) VALUES
 	 ('Exchanger','classes','java.util.concurrent',NULL),
 	 ('ExecutorCompletionService','classes','java.util.concurrent',NULL),
 	 ('Executors','classes','java.util.concurrent',NULL),
+	 ('Flow','classes','java.util.concurrent',NULL),
 	 ('ForkJoinPool','classes','java.util.concurrent',NULL),
 	 ('ForkJoinTask','classes','java.util.concurrent',NULL),
 	 ('ForkJoinWorkerThread','classes','java.util.concurrent',NULL),
@@ -64,6 +74,7 @@ INSERT INTO java_zoekterm (zoekterm,categorie,package,opmerking) VALUES
 	 ('RecursiveTask','classes','java.util.concurrent',NULL),
 	 ('ScheduledThreadPoolExecutor','classes','java.util.concurrent',NULL),
 	 ('Semaphore','classes','java.util.concurrent',NULL),
+	 ('SubmissionPublisher','classes','java.util.concurrent',NULL),
 	 ('SynchronousQueue','classes','java.util.concurrent',NULL),
 	 ('ThreadLocalRandom','classes','java.util.concurrent',NULL),
 	 ('ThreadPoolExecutor','classes','java.util.concurrent',NULL);
@@ -76,6 +87,19 @@ INSERT INTO java_zoekterm (zoekterm,categorie,package,opmerking) VALUES
 	 ('AtomicBoolean','classes','java.util.concurrent.atomic',NULL),
 	 ('AtomicInteger','classes','java.util.concurrent.atomic',NULL),
 	 ('AtomicLong','classes','java.util.concurrent.atomic',NULL),
+	 ('AtomicIntegerArray','classes','java.util.concurrent.atomic',NULL),
+	 ('AtomicIntegerFieldUpdater','classes','java.util.concurrent.atomic',NULL),
+	 ('AtomicLongArray','classes','java.util.concurrent.atomic',NULL),
+	 ('AtomicLongFieldUpdater','classes','java.util.concurrent.atomic',NULL),
+	 ('AtomicMarkableReference','classes','java.util.concurrent.atomic',NULL),
+	 ('AtomicReference','classes','java.util.concurrent.atomic',NULL),
+	 ('AtomicReferenceArray','classes','java.util.concurrent.atomic',NULL),
+	 ('AtomicReferenceFieldUpdater','classes','java.util.concurrent.atomic',NULL),
+	 ('AtomicStampedReference','classes','java.util.concurrent.atomic',NULL),
+	 ('DoubleAccumulator','classes','java.util.concurrent.atomic',NULL),
+	 ('DoubleAdder','classes','java.util.concurrent.atomic',NULL),
+	 ('LongAccumulator','classes','java.util.concurrent.atomic',NULL),
+	 ('LongAdder','classes','java.util.concurrent.atomic',NULL),
 	 ('AbstractOwnableSynchronizer','classes','java.util.concurrent.locks',NULL),
 	 ('AbstractQueuedLongSynchronizer','classes','java.util.concurrent.locks',NULL);
 INSERT INTO java_zoekterm (zoekterm,categorie,package,opmerking) VALUES
@@ -85,6 +109,7 @@ INSERT INTO java_zoekterm (zoekterm,categorie,package,opmerking) VALUES
 	 ('ReentrantReadWriteLock','classes','java.util.concurrent.locks',NULL),
 	 ('ReentrantReadWriteLock.ReadLock','classes','java.util.concurrent.locks',NULL),
 	 ('ReentrantReadWriteLock.WriteLock','classes','java.util.concurrent.locks',NULL),
+	 ('StampedLock','classes','java.util.concurrent.locks',NULL),
 	 ('Collections.synchronizedCollection()  	','classes','java.util.collections ',NULL),
 	 ('Collections.synchronizedList()  	','classes','java.util.collections ',NULL),
 	 ('Collections.synchronizedMap()  	','classes','java.util.collections ',NULL),
@@ -95,9 +120,16 @@ INSERT INTO java_zoekterm (zoekterm,categorie,package,opmerking) VALUES
 	 ('io.reactivex.rxjava2','libraries',NULL,'RxJava'),
 	 ('org.awaitility','libraries',NULL,'Awaitility'),
 	 ('io.projectreacto','libraries',NULL,'ProjectReactor'),
-	 ('com.lmax','libraries',NULL,'Disruptor');
+	 ('com.lmax','libraries',NULL,'Disruptor'),
+	 ('@Lock','libraries','javax.ejb','J2EE'),
+	 ('@ConcurrencyManagement','libraries','javax.ejb','J2EE'),
+	 ('SwingWorker','classes' ,'javax.swing',NULL),
+	 ('AsyncBoxView.ChildState','classes','javax.swing.text',NULL),
+	 ('RenderableImageProducer','classes','java.awt.image.renderable',NULL),
+	 ('TimerTask','classes','java.util',NULL);
 
-	
 -- vul zoekterm vanuit java zoektermen lijst	
 insert into zoekterm ( extensie, zoekwoord)
 select '.java', j.zoekterm  from java_zoekterm j;
+
+select count(*) from zoekterm;
