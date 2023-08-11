c4578c8257f774036d9a8dd7d9348d219d251f8d
chrisadoorn <chris.adoorn@gmail.com>
2023-08-11 09:17:39 +0200

    Queries die helpen SQ2 te beantwoorden bijgewerkt en aangevuld

--- database/queries/SQ2_query.sql
+++ database/queries/SQ2_query.sql
@@ -1,20 +1,37 @@
 --SQ.2 What is the correlation between multi-core programming primitives and the
 -- percentage of programmers using them?
 
--- Query:
-select b.author_id, count(distinct(d.zoekterm)) as verschillende_zoektermen
-	from project a,
-     commitinfo b,
-     bestandswijziging c,
-     bestandswijziging_zoekterm d
-	where a.id = b.idproject
-        and b.id = c.idcommit
-        and c.id = d.idbestandswijziging
-group by b.author_id
-order by verschillende_zoektermen desc;
+-- hoe vaak wordt welke zoekterm gebruikt?
+select wl.zoekterm , count(wl.zoekterm) as aantal_gebruik 
+from wijziging_lineage wl
+where wl.falsepositive = false 
+and   wl.uitgesloten = false 
+group by wl.zoekterm
+order by aantal_gebruik desc;
 
--- herschreven met gebruik van de view:
+-- door hoeveel auteurs wordt een zoekterm gebruikt?
+select wl.zoekterm , count(distinct wl.auteur) as aantal_auteurs 
+from wijziging_lineage wl
+where wl.falsepositive = false 
+and   wl.uitgesloten = false 
+group by wl.zoekterm
+order by aantal_auteurs desc;
+
+-- hoeveel verschillende zoektermen gebruikt een enkele auteur?
 select wl.auteur, count(distinct zoekterm) as verschillende_zoektermen
-from wijziging_lineage wl 
+from wijziging_lineage wl
+where wl.falsepositive = false 
+and   wl.uitgesloten = false
 group by wl.auteur
 order by verschillende_zoektermen desc;
+
+-- aantal auteurs die x verschillende zoektermen gebruiken. 
+select sq.verschillende_zoektermen, count(sq.verschillende_zoektermen)
+from ( select wl.auteur as auteur, count(wl.auteur)as aantal_gebruik ,count(distinct zoekterm) as verschillende_zoektermen 
+		from wijziging_lineage wl 
+		where wl.falsepositive = false 
+		and   wl.uitgesloten = false
+		group by wl.auteur) as sq
+group by sq.verschillende_zoektermen
+order by sq.verschillende_zoektermen asc;
+
