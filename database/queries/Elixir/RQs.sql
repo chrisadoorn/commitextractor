067c3f9eee344e11a59c90e9132fee3d8960b57e
evkammen <evert@evkammen.nl>
2023-08-24 00:13:38 +0200

    queries en plots

--- database/queries/Elixir/RQs.sql
+++ database/queries/Elixir/RQs.sql
@@ -44,11 +44,21 @@ insert into bw_met_zoektermen(idbestandswijziging)
 select distinct idbestandswijziging from bestandswijziging_zoekterm where falsepositive = false and aantalgevonden_nieuw > bestandswijziging_zoekterm.aantalgevonden_oud;
 
 
-select ci.author_id authorid, count(distinct ci.id) commitid, count(distinct ci.idproject) projectid,count(distinct bw.id) bwid, count(distinct bwz.idbestandswijziging) with_mc  from commitinfo ci
-left join bestandswijziging bw on ci.id = bw.idcommit
-left join bw_met_zoektermen bwz on bw.id = bwz.idbestandswijziging
-where ci.author_id < 900000000
-group by authorid order by with_mc;
+
+select count(xxx.authorid),
+       count(xxx.with_mc) filter(where xxx.with_mc > 0) as nr_mc, count(xxx.with_mc) filter(where xxx.with_mc = 0) as nr_no_mc
+    from
+(select ci.author_id authorid,
+       count(distinct ci.id) nr_commits,
+       count(distinct ci.idproject) nr_projects,
+       count(distinct bw.id) nr_bestands_wijzigingen,
+       count(distinct bwz.idbestandswijziging) with_mc
+from commitinfo ci
+         left join bestandswijziging bw on ci.id = bw.idcommit
+         left join bw_met_zoektermen bwz on bw.id = bwz.idbestandswijziging
+where ci.commitdatumtijd >= '2023-01-01' and ci.commitdatumtijd < '2024-01-01'
+group by authorid) as xxx;
+
 
 --
 
@@ -94,20 +104,51 @@ group by pr.id, ci.author_id
          order by pr.id;
 
 
-select xxx.id, xxx.aantal_commits,xxx.naam, count(xxx.author_id), count(xxx.cc)filter(where xxx.cc = 0) nr_no_mc, count(xxx.cc)filter(where xxx.cc > 0) nr_mc from
-(select pr.id, pr.aantal_commits, pr.naam, author_id, count(distinct bzztmt.zoekterm) as cc from project pr
+---
+set schema 'v11';
+CREATE TEMPORARY TABLE IF NOT EXISTS bw_met_zoektermen
+(
+    idbestandswijziging           integer           NOT NULL
+);
+
+truncate table bw_met_zoektermen;
+
+insert into bw_met_zoektermen(idbestandswijziging)
+select distinct idbestandswijziging from bestandswijziging_zoekterm where falsepositive = false and aantalgevonden_nieuw > bestandswijziging_zoekterm.aantalgevonden_oud;
+
+
+
+
+select xxx.id, xxx.aantal_commits,xxx.naam, count(xxx.author_id) as total_p,
+       count(xxx.author_id)filter(where xxx.cc = 0) as nr_no_mc,
+       count(xxx.author_id)filter(where xxx.cc > 0) as nr_mc,
+       count(xxx.author_id)filter(where xxx.cc > 0)/count(xxx.author_id)::float*100 as perc_mc
+from
+(select pr.id, pr.aantal_commits, pr.naam, author_id, count(distinct bzztmt.idbestandswijziging) as cc from project pr
 left join commitinfo ci on ci.idproject = pr.id
-left join bw_met_zoektermen_met_term bzztmt on bzztmt.idbestandswijziging = ci.id
+left join bestandswijziging bw on ci.id = bw.idcommit
+left join bw_met_zoektermen bzztmt on bzztmt.idbestandswijziging = bw.id
 group by pr.id, ci.author_id) as xxx
 group by id, aantal_commits, naam
 order by id;
 
 
 
+---
 
 
+select pr.id, pr.naam, author_id,count(distinct bzztmt.idbestandswijziging) from project pr
+left join commitinfo ci on ci.idproject = pr.id
+left join bestandswijziging bw on ci.id = bw.idcommit
+left join bw_met_zoektermen bzztmt on bzztmt.idbestandswijziging = bw.id
+group by pr.id, ci.author_id
+order by pr.id;
 
-
+select pr.id, pr.aantal_commits, pr.naam, author_id, count(distinct bzztmt.idbestandswijziging) as cc from project pr
+left join commitinfo ci on ci.idproject = pr.id
+left join bestandswijziging bw on ci.id = bw.idcommit
+left join bw_met_zoektermen bzztmt on bzztmt.idbestandswijziging = bw.id
+group by pr.id, ci.author_id;
 
 
 
@@ -138,11 +179,24 @@ select idbestandswijziging, zoekterm  from bestandswijziging_zoekterm where fals
 select zoekterm, count(idbestandswijziging) from bw_met_zoektermen_met_term
 group by zoekterm;
 
-select
-zoekterm, count(distinct ci.author_id)
+select zoekterm, count(distinct ci.author_id)
 from bw_met_zoektermen_met_term bzztmt
 join bestandswijziging bw on bzztmt.idbestandswijziging = bw.id
 join commitinfo ci  on ci.id = bw.idcommit
 group by zoekterm
-order by  zoekterm
+order by  zoekterm;
+
+
+--SQ.3 How has the usage of multi-core programming primitives changed over time? Is there a trend?
+
+select ci.id, ci.idproject, ci.commitdatumtijd,  ci.author_id,count(distinct bwz.idbestandswijziging) from commitinfo ci
+left join bestandswijziging bw on ci.id = bw.idcommit
+left join bw_met_zoektermen bwz on bw.id = bwz.idbestandswijziging
+group by ci.id
+order by idproject;
+
+SELECT date_trunc('month', commitdatumtijd) AS txn_month, count(*), count(*) filter(where count = 0)
+    ,count(*) filter(where count > 0), count(*) filter(where count > 0)/count(*)::float*100 as perc
+FROM v11."Result_20_commits_date"
+GROUP BY txn_month order by txn_month ;
 
---
+++ src/plots/plots_elixir/projects_against_date.py
@@ -0,0 +1,202 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from peewee import *
+from scipy.optimize import curve_fit
+from scipy.special import expit
+
+from src.models.selection_models import pg_db_schema
+from src.utils import configurator
+
+
+def objective_straight_line(x1, a1, b1):
+    return a1 * x1 + b1
+
+
+def objective_quadratic(x1, a1, b1, c1):
+    return a1 * x1 * x1 + b1 * x1 + c1
+
+
+def objective_straight_line_expit(x1, a1, b1):
+    z = objective_straight_line(x1, a1, b1)
+    return 100 * expit(z)
+
+
+def with_expit(x):
+    return 100 * expit((x/10) - 5)
+
+
+def objective_quadratic_2(x1, a1, b1, c1, d1):
+    return a1 * x1 * x1 * x1 + b1 * x1 * x1 + c1 * x1 + d1
+
+
+def create_diagram2():
+    sql = \
+        """
+        SELECT date_trunc('month', commitdatumtijd) AS txn_month, count(*), count(*) filter(where count = 0)
+        ,count(*) filter(where count > 0), count(*) filter(where count > 0)/count(*)::float*100 as perc
+        FROM {sch}."Result_20_commits_date"
+        GROUP BY txn_month order by txn_month ;
+        """.format(sch=pg_db_schema)
+
+    params_for_db = configurator.get_database_configuration()
+    connection = PostgresqlDatabase('multicore', user=params_for_db.get('user'), password=params_for_db.get('password'),
+                                    host='localhost', port=params_for_db.get('port'))
+
+    cursor = connection.execute_sql(sql)
+
+    line_data_1 = []
+    line_data_2 = []
+    for (date, counter, no_mc, mc, perc) in cursor.fetchall():
+        year = date.strftime("%Y")
+        month = date.strftime("%m")
+        line_data_1.append((year + '-' + month, counter))
+        line_data_2.append((year + '-' + month, perc))
+
+    line_data_1 = dict(line_data_1)
+    line_data_2 = dict(line_data_2)
+    x_ax_labels = []
+    line1_y_values = []
+    line2_y_values = []
+    months = create_month_list(2012, 1, 2023, 6)
+    for m in months:
+        l1data = line_data_1.get(m)
+        l2data = line_data_2.get(m)
+        x_ax_labels.append(m)  # alle x as labels
+        line1_y_values.append(l1data)
+        line2_y_values.append(l2data)
+
+    fig, ax1 = plt.subplots(layout='constrained')
+    ax1.scatter(x_ax_labels, line1_y_values, color='r')
+    ax1.set_xticks(np.arange(0, len(x_ax_labels), 12))
+    ax1.set_xticklabels(x_ax_labels[::12], rotation=45)
+    ax1.set_xlabel('Months')
+    ax1.set_ylabel('Nr of commits', color='r')
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color = 'tab:blue'
+    ax2.set_ylabel('percentage having mc added', color=color)  # we already handled the x-label with ax1
+    ax2.scatter(x_ax_labels, line2_y_values, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    plt.show()
+
+
+def create_diagram_projects_commits_authors():
+    sql = "SELECT id, aantal_commits, naam, total_p, nr_no_mc, nr_mc, perc_mc " \
+          "FROM {sch}.projects_commits_authors " \
+          "order by aantal_commits ; ".format(sch=pg_db_schema)
+    params_for_db = configurator.get_database_configuration()
+    connection = PostgresqlDatabase('multicore', user=params_for_db.get('user'), password=params_for_db.get('password'),
+                                host='localhost', port=params_for_db.get('port'))
+    cursor = connection.execute_sql(sql)
+
+    xs = []
+    ys = []
+    zipped = []
+    for (pr_id, aantal_commits, naam, total_p, nr_no_mc, nr_mc, perc_mc) in cursor.fetchall():
+        xs += [aantal_commits]
+        ys += [round(perc_mc, 2)]
+        zipped.append((aantal_commits, round(perc_mc, 2)))
+
+    x_ax_labels = list(range(0, 13000))
+
+    fig, ax1 = plt.subplots(layout='constrained')
+    ax1.scatter(xs, ys, color='b')
+    ax1.set_xticks(np.arange(0, len(x_ax_labels), 1000))
+    ax1.set_xticklabels(x_ax_labels[::1000], rotation=45)
+    ax1.set_xlabel('number of commits per project')
+    ax1.set_ylabel('percentage authors adding mc')
+
+    chunks_list = chunks(x_ax_labels, 1000)
+    i = 0
+    for chunk in chunks_list:
+        y_line = []
+        v = 0
+        counted = 1
+        for (x, y) in zipped:
+            if x >= i and x < i + 1000:
+                v += y
+                counted += 1
+        average = (v / counted)
+        for x in chunk:
+            y_line.append(average)
+        plt.plot(chunk, y_line, '--', color='r')
+        i += 1000
+
+    plt.show()
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+
+def create_diagram_projects_nr_authors():
+    sql = "SELECT id, aantal_commits, naam, total_p, nr_no_mc, nr_mc, perc_mc " \
+          "FROM {sch}.projects_commits_authors " \
+          "order by total_p ; ".format(sch=pg_db_schema)
+    params_for_db = configurator.get_database_configuration()
+    connection = PostgresqlDatabase('multicore', user=params_for_db.get('user'), password=params_for_db.get('password'),
+                                    host='localhost', port=params_for_db.get('port'))
+    cursor = connection.execute_sql(sql)
+
+    xs = []
+    ys = []
+    zipped = []
+    for (pr_id, aantal_commits, naam, total_p, nr_no_mc, nr_mc, perc_mc) in cursor.fetchall():
+        xs.append(total_p)
+        ys.append(round(perc_mc, 2))
+        if total_p > 800 and total_p < 900:
+            x =1;
+        zipped.append((total_p, round(perc_mc, 2)))
+    x_ax_labels = list(range(0, 1200))
+
+    fig, ax1 = plt.subplots(layout='constrained')
+    ax1.scatter(xs, ys, color='b')
+    ax1.set_xticks(np.arange(0, len(x_ax_labels), 100))
+    ax1.set_xticklabels(x_ax_labels[::100], rotation=45)
+    ax1.set_xlabel('number of authors per project')
+    ax1.set_ylabel('percentage authors adding mc')
+
+    chunks_list = chunks(x_ax_labels, 100)
+    i = 0
+    for chunk in chunks_list:
+        y_line = []
+        v = 0
+        counted = 1
+        for (x, y) in zipped:
+            if i <= x < (i + 100):
+                if x> 800 and x < 900:
+                    x =1
+                v += y
+                counted += 1
+        average = (v / counted)
+        for x in chunk:
+            y_line.append(average)
+        plt.plot(chunk, y_line, '--', color='r')
+        i += 100
+    plt.show()
+
+
+
+def create_month_list(start_year, start_month, end_year, end_month):
+    month_list = []
+    for year in range(start_year, end_year + 1):
+        if year == start_year:
+            for month in range(start_month, 13):
+                month_list.append(str(year) + '-' + ('0' if month < 10 else '') + str(month))
+        elif year == end_year:
+            for month in range(1, end_month + 1):
+                month_list.append(str(year) + '-' + ('0' if month < 10 else '') + str(month))
+        else:
+            for month in range(1, 13):
+                month_list.append(str(year) + '-' + ('0' if month < 10 else '') + str(month))
+    return month_list
+
+
+if __name__ == '__main__':
+    # create_diagram_projects_commits_authors()
+    # create_diagram_projects_nr_authors()
+    create_diagram2()
